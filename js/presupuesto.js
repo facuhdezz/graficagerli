aef6004a236464d0219202b91dddc98d57bf8ced
FacuHdezz <facuhernandez2000@gmail.com>
2023-10-06 02:28:47 -0300

    continuando presupuesto

---
+++ js/presupuesto.js
@@ -0,0 +1,29 @@
+const tarjetas = document.getElementById('btnTarjetas');
+const boletas = document.getElementById('btnBoletas');
+const volantes = document.getElementById('btnVolantes');
+const adhesivos = document.getElementById('btnAdhesivos');
+const impresiones = document.getElementById('btnImpresiones');
+const otros = document.getElementById('btnOtros');
+const todosLosElementos = [tarjetas,boletas,volantes,adhesivos,impresiones,otros];
+const listGroup = document.getElementsByClassName('list-group-item');
+const lista = document.getElementById('list-group');
+const tipoDeTrabajo = document.getElementById('tipoDeTrabajo');
+
+console.log(listGroup);
+function llamador(){    
+    for (let i=0 ; i < listGroup.length ; i++){
+        let idElement = listGroup[i].id;
+        let elemento = document.getElementById(idElement);
+        elemento.addEventListener('click', () => {
+            tipoDeTrabajo.innerHTML = elemento.id.slice(3);
+            todosLosElementos.forEach((element) => {
+                document.getElementById(element.id + "div").classList.add('d-none');
+                element.classList.remove('active');                                
+            });
+            elemento.classList.add('active');
+            document.getElementById(elemento.id + "div").classList.remove('d-none');
+        });
+    }
+}
+
+llamador();
